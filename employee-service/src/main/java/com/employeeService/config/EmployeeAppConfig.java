c06d89cda8a25b93c0a046b45a4a8a8a61a2fef0
dashratikanta2001 <69645865+dashratikanta2001@users.noreply.github.com>
2024-01-30 11:55:19 +0530

    WebClient implemented

--- employee-service/src/main/java/com/employeeService/config/EmployeeAppConfig.java
+++ employee-service/src/main/java/com/employeeService/config/EmployeeAppConfig.java
@@ -1,13 +1,16 @@
 package com.employeeService.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class EmployeeAppConfig {
 
+	@Value("${addressservice.base.url}") 
+	private String addressBaseUrl;
 	
 //	@Bean
 //	public RestTemplate restTemplate()
@@ -19,5 +22,14 @@ public class EmployeeAppConfig {
 	public ModelMapper modelMapper()
 	{
 		return new ModelMapper();
+	}
+	
+	@Bean
+	public WebClient webClient()
+	{
+		return WebClient
+					.builder()
+					.baseUrl(addressBaseUrl)
+					.build();
 	}
 }
--- employee-service/src/main/java/com/employeeService/service/EmployeeService.java
+++ employee-service/src/main/java/com/employeeService/service/EmployeeService.java
@@ -2,10 +2,8 @@ package com.employeeService.service;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.employeeService.dao.EmployeeDao;
 import com.employeeService.entity.Employee;
@@ -18,18 +16,21 @@ public class EmployeeService {
 	@Autowired
 	private EmployeeDao employeeDao;
 	
-	private RestTemplate restTemplate;
+//	private RestTemplate restTemplate;
 	
 	@Autowired
 	private ModelMapper modelMapper;
 	
+	@Autowired
+	private WebClient webClient;
 	
-	public EmployeeService(@Value("${addressservice.base.url}") String addressBaseUrl,RestTemplateBuilder builder) {
-
-		this.restTemplate= builder
-							.rootUri(addressBaseUrl)
-							.build();
-	}
+	
+//	public EmployeeService(@Value("${addressservice.base.url}") String addressBaseUrl,RestTemplateBuilder builder) {
+//
+//		this.restTemplate= builder
+//							.rootUri(addressBaseUrl)
+//							.build();
+//	}
 	
 	public EmployeeResponse getEmployeeById(int id)
 	{
@@ -40,10 +41,20 @@ public class EmployeeService {
 		
 		EmployeeResponse employeeResponse = modelMapper.map(employee, EmployeeResponse.class);
 		
-		AddressResponse addressResponse = restTemplate.getForObject("/address/{id}", AddressResponse.class, id);
+		AddressResponse addressResponse = webClient
+												.get()
+												.uri("/address/"+id)
+												.retrieve()
+												.bodyToMono(AddressResponse.class)
+												.block();
 		
 		employeeResponse.setAddressResponse(addressResponse);
 		
 		return employeeResponse;
 	}
+
+//	private AddressResponse callingAddressServiceUsingRESTTemplate(int id) {
+//		// TODO Auto-generated method stub
+//		return 	restTemplate.getForObject("/address/{id}", AddressResponse.class, id);
+//	}
 }
